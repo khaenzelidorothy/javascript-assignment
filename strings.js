3066888d1ec8173719f634c8c92054104d650732
Dorothy <khaenzelidorothy@gmail.com>
2025-03-18 06:38:30 +0300

    strings

--- strings.js
+++ strings.js
@@ -5,13 +5,13 @@ console.log(word.slice(8));
  let constFood ='The quick fox jumped over the lazy dog';
 let newFood = constFood.slice(0,4) + 'eat' + constFood.slice(4);
 console.log(newFood);
-
+//question three
 let constStory= "The quick brown fox jumps over the lazy dog"
 function countOccurrences(text, word) {
     return text.toLowerCase().split(word.toLowerCase()).length - 1;}
     console.log(countOccurrences(constStory, "the"));
     console.log(countOccurrences(constStory, "brown"))
-
+//question four
  let constString1 ='The pupils are reading in the library';
 let word1 =constString1.includes('are'? 'are' : 'not included');
 console.log(word1);
@@ -20,7 +20,7 @@ console.log(word1);
 let constString2 ='The child was sitting at the table before it fell';
 let word2 =constString2.includes('sitting'? 'sitting' : 'not included');
 console.log(word2);
-
+//question five
 let x ='wonderful';
 let y='amazing';
 let w='UndERneath';
